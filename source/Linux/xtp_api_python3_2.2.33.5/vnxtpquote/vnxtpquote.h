924f70946afebd4dc83a2777a95b68457c425ab5
subaochen <subaochen@126.com>
2023-11-05 10:04:17 +0800

    修改mutex为boost::mutex，否则Linux下面无法编译

--- source/Linux/xtp_api_python3_2.2.33.5/vnxtpquote/vnxtpquote.h
+++ source/Linux/xtp_api_python3_2.2.33.5/vnxtpquote/vnxtpquote.h
@@ -113,7 +113,7 @@ class ConcurrentQueue
 {
 private:
 	queue<Data> the_queue;								//标准库队列
-	mutable mutex the_mutex;							//boost互斥锁
+	mutable boost::mutex the_mutex;							//boost互斥锁
 	condition_variable the_condition_variable;			//boost条件变量
 
 public:
@@ -121,7 +121,7 @@ public:
 	//存入新的任务
 	void push(Data const& data)
 	{
-		mutex::scoped_lock lock(the_mutex);				//获取互斥锁
+		boost::mutex::scoped_lock lock(the_mutex);				//获取互斥锁
 		the_queue.push(data);							//向队列中存入数据
 		lock.unlock();									//释放锁
 		the_condition_variable.notify_one();			//通知正在阻塞等待的线程
@@ -130,14 +130,14 @@ public:
 	//检查队列是否为空
 	bool empty() const
 	{
-		mutex::scoped_lock lock(the_mutex);
+		boost::mutex::scoped_lock lock(the_mutex);
 		return the_queue.empty();
 	}
 
 	//取出
 	Data wait_and_pop()
 	{
-		mutex::scoped_lock lock(the_mutex);
+		boost::mutex::scoped_lock lock(the_mutex);
 
 		while (the_queue.empty())						//当队列为空时
 		{
